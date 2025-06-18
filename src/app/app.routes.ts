6eed259f0c61e10b8842425b31d1e465ddbb8b11
Raulshadow <50181593+Raulshadow@users.noreply.github.com>
2025-06-18 14:18:34 -0300

    Adição da página de detalhes do Pokemon
    Adicionei uma página simples de detalhes do pokémon, ao se clicar no pokémon, há uma visualização das informações simples de um pokémon, essa tela será melhor implementada futuramente, conseguindo melhores detalhes do pokémon e melhor design.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'pokemon/:name',
+    loadComponent: () => import('./pages/pokemon-detail/pokemon-detail.page').then( m => m.PokemonDetailPage)
+  },
 ];
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -13,5 +13,10 @@ export class HttpService {
   getAllPokemonSimpleForm(limit: number = 1025) {
     // Esse método serve para buscar todos os pokémons de forma direta, pegando a versão base deles, sem mega evoluções ou outras variações.
     return this.http.get(`${this.api}/pokemon?limit=${limit}`);
-  } 
+  }
+
+  getPokemonByName(name: string) {
+    // Esse método serve para buscar um pokémon específico pelo nome.
+    return this.http.get(`${this.api}/pokemon/${name}`);
+  }
 }
