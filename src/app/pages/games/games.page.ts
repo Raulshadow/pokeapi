1b10a8468c2682339e4246c6141ddab3efd81744
Raulshadow <50181593+Raulshadow@users.noreply.github.com>
2025-06-19 15:09:52 -0300

    Atualização da página de Jogos Pokémon
    Adionado imagens .jpg vindas da internet, para exibir a capa dos Jogos.
    
    Adicionado formas de conseguir as versões dos jogos, somente existem 46 versões, mas algumas delas são japonesas repetidas ou até mesmos dlcs, filtrei elas a partir de que dlcs começam com "the" no inicio da string e retirei aquelas que contenham "japan" no texto.
    
    Repliquei a forma de listagem de regiões e de pokémons, adicionando imagem a lista para auxiliar.
    
    Hoje fui mais sucinto nas atualizações, é meu aniversário e feriado no país. Vou manter apenas essa atualização hoje, amanhã terminarei a estrutura total da aplicação, depois partirei para ícones e estilização.

--- src/app/pages/games/games.page.ts
+++ src/app/pages/games/games.page.ts
@@ -1,20 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonList, IonItem, IonAvatar } from '@ionic/angular/standalone';
+import { NgFor } from '@angular/common';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-games',
   templateUrl: './games.page.html',
   styleUrls: ['./games.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [NgFor, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonLabel, IonList, IonItem, IonAvatar],
 })
 export class GamesPage implements OnInit {
+  private httpService = inject(HttpService);
+  public gamesVersions: any[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.getAllGames();
   }
 
+  private getAllGames() {
+    this.httpService.getAllPokemonGames().subscribe((data: any) => {
+      this.gamesVersions = data.results.filter(
+        (game: { name: string; url: string }) => {
+          const name = game.name.toLowerCase();
+          return !name.startsWith('the') && !name.includes('japan');
+        }
+      );
+    });
+  }
+
+  formatGameName(name: string): string {
+    return 'Pokémon ' + name.replace(/-/g, ' ');
+  }
+
+  getGameImage(name: string): string {
+    // Retornando o caminho da imagem representativa da região.
+    return `assets/games/${name}.jpg`;
+  }
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -13,5 +13,10 @@ export class HttpService {
   getAllPokemonSimpleForm(limit: number = 1025) {
     // Esse método serve para buscar todos os pokémons de forma direta, pegando a versão base deles, sem mega evoluções ou outras variações.
     return this.http.get(`${this.api}/pokemon?limit=${limit}`);
-  } 
+  }
+
+  getAllPokemonGames(limit: number = 47) {
+    // Esse método serve para buscar todos os as versões dos jogos da franquia. Infelizmente ele ainda vem com dlcs e spinoffs, tratar no games.ps.
+    return this.http.get(`${this.api}/version?limit=${limit}`);
+  }
 }
